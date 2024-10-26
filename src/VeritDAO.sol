f39ac2ecb006bdae58d0c2ee9da6e99f34bfe9ea
neetance <ankitbasu14@gmail.com>
2024-10-27 00:25:40 +0530

    wrote the instance contract and made updates in the dao contract

--- src/VeritDAO.sol
+++ src/VeritDAO.sol
@@ -9,15 +9,20 @@ contract VeritDAO is Ownable {
     error Can_Only_Vote_Once();
     error Deadline_Reached();
     error Voting_Ongoing();
+    error Already_Executed();
+
+    event NewVote(address indexed voter, uint256 severity, bool voteFor);
 
     uint256 startTime;
     uint256 public deadline;
-    uint256 posVotes;
-    uint256 negVotes;
+    uint256 public posVotes;
+    uint256 public negVotes;
+    uint256 public severityScore;
+    uint256 public impactLoss;
 
     address public claimer;
     address pool;
-    address private admin;
+    address[] public voters;
 
     mapping(address => bool) hasVoted;
 
@@ -29,7 +34,11 @@ contract VeritDAO is Ownable {
 
     State state;
 
-    constructor(address _claimer, address poolAddr) Ownable(admin) {
+    constructor(
+        address _claimer,
+        address poolAddr,
+        uint256 _impactLoss
+    ) Ownable(msg.sender) {
         claimer = _claimer;
         startTime = block.timestamp;
         deadline = block.timestamp + 7 days;
@@ -37,6 +46,8 @@ contract VeritDAO is Ownable {
         pool = poolAddr;
         posVotes = 0;
         negVotes = 0;
+        severityScore = 0;
+        impactLoss = _impactLoss;
     }
 
     modifier onlyDAOMember(address voter) {
@@ -54,22 +65,37 @@ contract VeritDAO is Ownable {
         _;
     }
 
-    function voteFor() public onlyDAOMember(msg.sender) ongoing notVoted {
+    function voteFor(
+        uint256 severity
+    ) public onlyDAOMember(msg.sender) ongoing notVoted {
         hasVoted[msg.sender] = true;
         uint256 balance = IERC20(pool).balanceOf(msg.sender);
         posVotes += balance;
+        severityScore += severity;
+
+        voters.push(msg.sender);
+        emit NewVote(msg.sender, severity, true);
     }
 
     function voteAgainst() public onlyDAOMember(msg.sender) ongoing notVoted {
         hasVoted[msg.sender] = true;
         uint256 balance = IERC20(pool).balanceOf(msg.sender);
         negVotes += balance;
+
+        voters.push(msg.sender);
+        emit NewVote(msg.sender, 0, false);
     }
 
-    function execute() external onlyOwner {
+    function execute() external onlyOwner returns (bool) {
         if (block.timestamp < deadline) revert Voting_Ongoing();
+        if (state != State.ONGOING) revert Already_Executed();
 
-        if (negVotes > posVotes) state = State.REJECTED;
-        else state = State.APPROVED;
+        if (negVotes > posVotes) {
+            state = State.REJECTED;
+            return false;
+        } else {
+            state = State.APPROVED;
+            return true;
+        }
     }
 }
--- src/VeritInstance.sol
+++ src/VeritInstance.sol
@@ -1,14 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {VeritDAO} from "./VeritDAO.sol";
+
 contract VeritInstance {
+    error AlreadyClaimed();
+    error DeadlinePassed();
+    error NotHolder();
+
     address private immutable holder;
+    address pool;
     uint256 private immutable premium;
     uint256 private immutable deadline;
+    bool public claimed;
+
+    VeritDAO dao;
 
     constructor(address _holder, uint256 _premium, uint256 _deadline) {
         holder = _holder;
         premium = _premium;
         deadline = _deadline;
+        claimed = false;
+    }
+
+    modifier onlyHolder() {
+        if (msg.sender != holder) revert NotHolder();
+        _;
+    }
+
+    function claim(uint256 lossAmount) public onlyHolder returns (address) {
+        if (claimed) {
+            revert AlreadyClaimed();
+        }
+        if (block.timestamp >= deadline) {
+            revert DeadlinePassed();
+        }
+
+        dao = new VeritDAO(holder, pool, lossAmount);
+        return address(dao);
+    }
+
+    function redeem() public onlyHolder returns (bool) {
+        if (claimed) {
+            revert AlreadyClaimed();
+        }
+        if (block.timestamp < deadline) {
+            revert DeadlinePassed();
+        }
+
+        claimed = true;
+        return dao.execute();
     }
 }
